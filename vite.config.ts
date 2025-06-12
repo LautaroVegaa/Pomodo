40d9bf02646f014b009523a003e422a460602274
Lautaro Vega <lautivegacarignani@gmail.com>
2025-06-12 11:56:57 -0300

    Update configuration files and HTML

--- vite.config.ts
+++ vite.config.ts
@@ -22,24 +22,7 @@ export default defineConfig(({ mode }) => ({
         start_url: '/',
         display: 'standalone',
         background_color: '#ffffff',
-        theme_color: '#1f2937',
-        icons: [
-          {
-            src: '/pwa-icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/pwa-icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: '/apple-touch-icon.png',
-            sizes: '180x180',
-            type: 'image/png'
-          }
-        ]
+        theme_color: '#1f2937'
       }
     })
   ].filter(Boolean),
